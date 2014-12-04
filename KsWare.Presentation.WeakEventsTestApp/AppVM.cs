83271c8204ef0ab8cd0828692677f6bad0f91202
KUX <ks@ksware.de>
2014-12-04 15:23:31 +0100

    KsWare.Presentation.WeakEventsTestApp project added

---
+++ KsWare.Presentation.WeakEventsTestApp/AppVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using KsWare.Presentation.ViewModelFramework;
+
+namespace KsWare.Presentation.WeakEventsTestApp {
+
+	public class AppVM:ApplicationVM {
+
+		public AppVM() {
+			
+		}
+
+		protected override void OnStartup(StartupEventArgs e) {
+			base.OnStartup(e);
+			MainWindowVM.Instance.Show();
+		}
+
+	}
+}
---
+++ KsWare.Presentation.WeakEventsTestApp/MainWindowVM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using KsWare.Presentation.InternalStatistics;
+using KsWare.Presentation.ViewModelFramework;
+
+namespace KsWare.Presentation.WeakEventsTestApp {
+
+	public class MainWindowVM:WindowVM {
+
+		static MainWindowVM s_Instance;
+		private TestViewModel1VM m_TestViewModel;
+		private WeakReference m_WeakTestViewModel;
+
+		public static MainWindowVM Instance {get { return s_Instance ?? (s_Instance = new MainWindowVM()); } }
+
+		public MainWindowVM() {
+
+			var wo = new WeakReference(new object());
+			if (wo.IsAlive) { /*alles in Ordnung*/ }
+			GC.Collect(); 
+			if(wo.IsAlive) throw new Exception("GC.Collec() failed!??"); // Debug x86
+
+			StatisticsWindowVM.Instance.Show();
+			RegisterChildren(_=>this);
+
+			CreateAction.MːDoAction = () => {
+				m_WeakTestViewModel=new WeakReference(m_TestViewModel = new TestViewModel1VM());
+//				DoForget=m_TestViewModel.IsSelectedChangedEvent;
+				m_TestViewModel.IsSelectedChangedEvent.Register(null, "AtIsSelectedChanged",AtIsSelectedChangedEvent);
+			};
+			DeleteAction.MːDoAction = () => {
+//				m_TestViewModel.IsSelectedChangedEvent.Release(this,"AtIsSelectedChanged");
+//				m_TestViewModel.Dispose();
+				m_TestViewModel = null; 
+				GC.Collect(); 
+				EventUtil.WeakEventManager.Collect();
+				if(m_WeakTestViewModel.IsAlive) Debug.WriteLine("m_WeakTestViewModel.IsAlive");
+			};
+		}
+
+		private void DoNothing(object o) {  }
+		private object DoForget { set { } }
+
+		private void AtIsSelectedChangedEvent(object sender, EventArgs eventArgs) {  }
+
+		public ActionVM CreateAction { get; private set; }
+		public ActionVM DeleteAction { get; private set; }
+
+	}
+
+	public class TestViewModel1VM:ObjectVM {
+
+		public TestViewModel1VM() {
+			//this.IsSelectedChangedEvent.Register(null, "AtIsSelectedChanged",AtIsSelectedChangedEvent);
+		}
+
+		~TestViewModel1VM() {
+			Dispose(false);
+		}
+
+
+		protected override void Dispose(bool explicitDisposing) {
+			Debug.WriteLine("TestViewModel1VM Dispose "+explicitDisposing);
+			base.Dispose(explicitDisposing);
+		}
+
+		private void AtIsSelectedChangedEvent(object sender, EventArgs eventArgs) {  }
+
+	}
+
+}
