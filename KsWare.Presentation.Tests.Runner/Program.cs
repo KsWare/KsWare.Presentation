b2ec77e1c1b0b25b0661246288908c728ac8aafe
KUX <ks@ksware.de>
2014-12-04 15:19:33 +0100

    KsWare.Presentation.Tests.Runner project added

---
+++ KsWare.Presentation.Tests.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KsWare.Presentation1.Tests.Runner {
+
+	static class Program {
+
+		[STAThread]
+		static void Main(string[] args) {
+			KsWare.Presentation.Tests.Program.Main(args);
+		}
+	}
+}
