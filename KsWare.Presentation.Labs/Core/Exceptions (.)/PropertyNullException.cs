1021d99f7c9248136b314efdb1ef793be5b07f43
SchreinerK <SchreinerK.GitHub@ksware.de>
2016-02-16 18:32:05 +0100

    missing files added (folders starting with ~ are ignored by default)

---
+++ KsWare.Presentation.Labs/Core/Exceptions (.)/PropertyNullException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+using JetBrains.Annotations;
+
+namespace KsWare.Presentation {
+
+	[SerializableAttribute]
+	public class PropertyNullException : InvalidOperationException /*ArgumentNullException*/ {
+		private const string enM = "Property cannot be null.";
+		private const string enP = "Property name";
+
+		public PropertyNullException():base("Property cannot be null.") {}
+
+		public PropertyNullException(string propertyName) : base(FormatMessage(propertyName)) {
+			PropertyName = propertyName;
+		}
+
+		public PropertyNullException(string message, Exception innerException) : base(message, innerException) {}
+
+		public PropertyNullException(string propertyName, string message) : base(FormatMessage(propertyName, message)) {
+			PropertyName = propertyName;
+		}
+
+		protected PropertyNullException([NotNull] SerializationInfo info, StreamingContext context) : base(info, context) {}
+
+		private static string FormatMessage(string propertyName) {
+			return string.Format("{0}\r\n{1}: {2}", enM,enP, propertyName);
+		}
+
+		private static string FormatMessage(string propertyName, string message) {
+			return string.Format("{0}\r\n{1}: {2}", message,enP, propertyName);
+		}
+
+		public string PropertyName { get; private set; }
+	}
+
+}
