155dec7f718b39d6dbe73e16c4a7895e913b35c9
SchreinerK <SchreinerK.GitHub@ksware.de>
2016-06-20 19:09:02 +0200

    test fixed

--- KsWare.Presentation.ViewModelFramework/ViewModel/(OtherVMs)/ActionVM.cs
+++ KsWare.Presentation.ViewModelFramework/ViewModel/(OtherVMs)/ActionVM.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Windows.Input;
 using JetBrains.Annotations;
 using KsWare.Presentation.BusinessFramework;
