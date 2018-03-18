9c28e04f0b3a5e5e11cf0d7a68703738e2f9be76
%github_user_name_appveyor% <%github_user_email_appveyor%>
2018-03-18 02:59:59 -0700

    [skip ci] increment version 0.18.1+20180318025829

--- src/KsWare.Presentation.Core.CLR452.Tests/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.Core.CLR452.Tests/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18.0")]
-[assembly: AssemblyVersion("0.18.0")]
-[assembly: AssemblyFileVersion("0.18.0")]
-[assembly: AssemblyInformationalVersion("0.18.0")]
+// [assembly: AssemblyVersion("0.18.1")]
+[assembly: AssemblyVersion("0.18.1")]
+[assembly: AssemblyFileVersion("0.18.1")]
+[assembly: AssemblyInformationalVersion("0.18.1")]
