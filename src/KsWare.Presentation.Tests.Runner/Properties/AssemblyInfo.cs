684a6d448887177e71af162de8f3b14ff28c5cc4
Appveyor-KsWare <appveyor@ksware.de>
2018-03-18 05:09:04 -0700

    [skip ci] increment version 0.18.4+20180318050715

--- src/KsWare.Presentation.Tests.Runner/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.Tests.Runner/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18.3")]
-[assembly: AssemblyVersion("0.18.3")]
-[assembly: AssemblyFileVersion("0.18.3")]
-[assembly: AssemblyInformationalVersion("0.18.3+20180318042901")]
+// [assembly: AssemblyVersion("0.18.4")]
+[assembly: AssemblyVersion("0.18.4")]
+[assembly: AssemblyFileVersion("0.18.4")]
+[assembly: AssemblyInformationalVersion("0.18.4+20180318050715")]
--- src/KsWare.Presentation.ViewModelFramework/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.ViewModelFramework/Properties/AssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: Guid("e45e9672-0236-47fc-9ade-5fef5cdfc7ae")]
 
-[assembly: AssemblyVersion("0.18.3")]
-[assembly: AssemblyFileVersion("0.18.3")]
-[assembly: AssemblyInformationalVersion("0.18.3+20180318042901")]
+[assembly: AssemblyVersion("0.18.4")]
+[assembly: AssemblyFileVersion("0.18.4")]
+[assembly: AssemblyInformationalVersion("0.18.4+20180318050715")]
 
 [assembly: XmlnsDefinition(KsWare.Presentation.ViewModelFramework.AssemblyInfo.XmlNamespace, "KsWare.Presentation.ViewModelFramework")]
 [assembly: XmlnsPrefix("http://ksware.de/Presentation/ViewModelFramework", "ksvm")]
