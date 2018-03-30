4e12ce0a8f47fb0bf4d05f608ff1943571bcf1a2
Appveyor-KsWare <appveyor@ksware.de>
2018-03-30 06:52:11 -0700

    [skip ci] increment version 0.18.17+20180330065030

--- src/KsWare.Presentation.ViewModelFramework/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.ViewModelFramework/Properties/AssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: Guid("e45e9672-0236-47fc-9ade-5fef5cdfc7ae")]
 
-[assembly: AssemblyVersion("0.18.16")]
-[assembly: AssemblyFileVersion("0.18.16")]
-[assembly: AssemblyInformationalVersion("0.18.16")]
+[assembly: AssemblyVersion("0.18.17")]
+[assembly: AssemblyFileVersion("0.18.17")]
+[assembly: AssemblyInformationalVersion("0.18.17")]
 
 [assembly: XmlnsDefinition(KsWare.Presentation.ViewModelFramework.AssemblyInfo.XmlNamespace, "KsWare.Presentation.ViewModelFramework")]
 [assembly: XmlnsPrefix("http://ksware.de/Presentation/ViewModelFramework", "ksvm")]
--- src/KsWare.Presentation.WeakEventsTestApp/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.WeakEventsTestApp/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.18.16")]
-[assembly: AssemblyFileVersion("0.18.16")]
-[assembly: AssemblyInformationalVersion("0.18.16")]
+[assembly: AssemblyVersion("0.18.17")]
+[assembly: AssemblyFileVersion("0.18.17")]
+[assembly: AssemblyInformationalVersion("0.18.17")]
