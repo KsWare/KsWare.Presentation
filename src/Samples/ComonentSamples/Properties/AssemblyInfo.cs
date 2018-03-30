9605e3be96ec0e0777bc2f2160beeb62cb5f1a08
Appveyor-KsWare <appveyor@ksware.de>
2018-03-30 05:48:24 -0700

    [skip ci] increment version 0.18.16+20180330054648

--- src/Samples/ComonentSamples/Properties/AssemblyInfo.cs
+++ src/Samples/ComonentSamples/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18.15")]
-[assembly: AssemblyVersion("0.18.15")]
-[assembly: AssemblyFileVersion("0.18.15")]
+// [assembly: AssemblyVersion("0.18.16")]
+[assembly: AssemblyVersion("0.18.16")]
+[assembly: AssemblyFileVersion("0.18.16")]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation","KsWare.Presentation.ComponentSamples.DocumentElements")]
--- src/Samples/SimpleMVVMApplication/Properties/AssemblyInfo.cs
+++ src/Samples/SimpleMVVMApplication/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18.15")]
-[assembly: AssemblyVersion("0.18.15")]
-[assembly: AssemblyFileVersion("0.18.15")]
+// [assembly: AssemblyVersion("0.18.16")]
+[assembly: AssemblyVersion("0.18.16")]
+[assembly: AssemblyFileVersion("0.18.16")]
