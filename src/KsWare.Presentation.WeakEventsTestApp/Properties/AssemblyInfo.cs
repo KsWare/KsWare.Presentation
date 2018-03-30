3089a43755d4673519d964f160e062fc975db413
Appveyor-KsWare <appveyor@ksware.de>
2018-03-30 10:00:01 -0700

    [skip ci] increment version 0.18.20+20180330095813

--- src/KsWare.Presentation.WeakEventsTestApp/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.WeakEventsTestApp/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.18.19")]
-[assembly: AssemblyFileVersion("0.18.19")]
-[assembly: AssemblyInformationalVersion("0.18.19")]
+[assembly: AssemblyVersion("0.18.20")]
+[assembly: AssemblyFileVersion("0.18.20")]
+[assembly: AssemblyInformationalVersion("0.18.20")]
