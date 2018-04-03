213dd61968de0fb53a8c677b7262c0f9b2d9efd8
Appveyor-KsWare <appveyor@ksware.de>
2018-04-03 06:55:45 -0700

    [skip ci] increment version 0.18.23+20180403065353

--- src/KsWare.Presentation.ViewFramework/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.ViewFramework/Properties/AssemblyInfo.cs
@@ -32,10 +32,10 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.18.22")]
-[assembly: AssemblyVersion("0.18.22")]
-[assembly: AssemblyFileVersion("0.18.22")]
-[assembly: AssemblyInformationalVersion("0.18.22")]
+// [assembly: AssemblyVersion("0.18.23")]
+[assembly: AssemblyVersion("0.18.23")]
+[assembly: AssemblyFileVersion("0.18.23")]
+[assembly: AssemblyInformationalVersion("0.18.23")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
