feeee2502cfdf446adcfe99dc5545e668bc2bcc5
Appveyor-KsWare <appveyor@ksware.de>
2019-09-03 21:10:33 +0000

    [skip ci] increment version 0.18.38+20190903210858

--- src/KsWare.Presentation.Labs/Properties/AssemblyInfo.cs
+++ src/KsWare.Presentation.Labs/Properties/AssemblyInfo.cs
@@ -17,9 +17,9 @@ using System.Windows.Markup;
 [assembly: ComVisible(false)]
 [assembly: Guid("b2428431-ad89-4221-8b7c-4109afa93690")]
 
-[assembly: AssemblyVersion("0.18.36")]
-[assembly: AssemblyFileVersion("0.18.36")]
-[assembly: AssemblyInformationalVersion("0.18.36")]
+[assembly: AssemblyVersion("0.18.38")]
+[assembly: AssemblyFileVersion("0.18.38")]
+[assembly: AssemblyInformationalVersion("0.18.38")]
 
 [assembly: XmlnsPrefix(KsWare.Presentation.Labs.AssemblyInfo.XmlNamespace, "kslab")]
 [assembly: XmlnsDefinition(KsWare.Presentation.Labs.AssemblyInfo.XmlNamespace, "KsWare.Presentation.ViewFramework")]
