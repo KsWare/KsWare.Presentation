c3a4bd64486547d1a2b20d486221715115d5dbb8
SchreinerK <ks@ksware.de>
2019-09-03 17:31:58 +0200

    nuget KsWare.Presentation.ViewModelFramework.Common

---
+++ src/KsWare.Presentation.UITestApp/Pages/VisibilityBindingTests.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows.Controls;
+using KsWare.Presentation.ViewModelFramework;
+
+namespace KsWare.Presentation.UITestApp.Pages
+{
+	/// <summary>
+	/// Interaction logic for VisibilityBindingTests.xaml
+	/// </summary>
+	public partial class VisibilityBindingTests : Page
+	{
+		public VisibilityBindingTests()
+		{
+			InitializeComponent();
+		}
+
+
+	}
+		public class VisibilityBindingTests_MyDataContext : ObjectVM
+		{
+			private bool _boolValue;
+			private int _intValue;
+
+			public bool BoolValue {
+				get => _boolValue;
+				set {
+					if (Equals(_boolValue, value)) return;
+					_boolValue = value;
+					OnPropertyChanged("BoolValue");
+				}
+			}
+			public int IntValue {
+				get => _intValue;
+				set {
+					if (Equals(_intValue, value)) return;
+					_intValue = value;
+					OnPropertyChanged("IntValue");
+				}
+			}
+	}
+
+}
