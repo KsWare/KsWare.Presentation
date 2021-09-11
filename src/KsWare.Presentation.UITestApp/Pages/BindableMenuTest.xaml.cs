b788360f665dbc6f4626be7f6828ffbb95457267
SchreinerK <ks@ksware.de>
2021-09-11 17:26:43 +0200

    MenuItemVM.Command

---
+++ src/KsWare.Presentation.UITestApp/Pages/BindableMenuTest.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JetBrains.Annotations;
+using KsWare.Presentation.ViewModelFramework;
+
+namespace KsWare.Presentation.UITestApp.Pages {
+	/// <summary>
+	/// Interaction logic for BindableMenuTest.xaml
+	/// </summary>
+	public partial class BindableMenuTest : Page {
+		public BindableMenuTest() {
+			InitializeComponent();
+		}
+	}
+
+	public class BindableMenuTestVM:ObjectVM{
+
+		/// <inheritdoc />
+		public BindableMenuTestVM() {
+			RegisterChildren(() => this);
+
+			MenuItemVM test,more,check;
+			Menu.Add(test = new MenuItemVM { Caption = "_Test" });
+			test.Items.Add(new MenuItemVM { Caption = "ActionCommand", CommandAction = { MːExecutedCallback = DoCommand } });
+			test.Items.Add(new MenuItemVM { Caption = "Cut", Command = ApplicationCommands.Cut });
+			test.Items.Add(new MenuItemVM { Caption = "-" });
+			test.Items.Add(more = new MenuItemVM { Caption = "More" });
+			more.Items.Add(check = new MenuItemVM { Caption = "Checker", IsCheckable = true });
+			more.Items.Add(new MenuItemVM { Caption = "Dummy" });
+
+			// #1 synchronize 2 bool values (functional, not optimized for reuse)
+			// Fields[nameof(IsChecked)].ValueChangedEvent.add = (s, e) => check.IsChecked = (bool)e.NewValue;
+			// check.Fields[nameof(MenuItemVM.IsChecked)].ValueChangedEvent.add = (s, e) => IsChecked = (bool)e.NewValue;
+			// #2 synchronize 2 bool values (experimental)
+			FieldBindingOperations.SetBinding(Fields[nameof(IsChecked)], new FieldBinding(check.Fields[nameof(MenuItemVM.IsChecked)]));
+		}
+
+		private void DoCommand(object? sender, ExecutedEventArgs e) {
+			MessageBox.Show($"ActionCommand {e.Parameter}");
+		}
+
+		public ListVM<MenuItemVM> Menu { get; [UsedImplicitly] private set; }
+
+		public bool IsChecked { get => Fields.GetValue<bool>(); set => Fields.SetValue(value); }
+	}
+}
